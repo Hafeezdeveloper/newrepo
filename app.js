c00dc671f47ea9163a165a23e9b60b77cf446015
hafeezdeveloper <hafeez.hussain2003@gmail.com>
2024-07-14 16:37:33 +0500

    inital

---
+++ app.js
@@ -0,0 +1,21 @@
+//     // Input: nums = [5, 2, 3, 1]
+//     // Output: [1, 2, 3, 5]
+
+// let arr = []
+// var sortArray = function (nums) {
+
+//     for (var i = 0; i < nums.length; i++) {
+//         if (i === 0 || i === nums.length - 1) {
+//             arr.push(nums[i])
+//             console.log( "awd",nums[i])
+//         }
+//     }
+//     console.log("arr ", arr)
+//     nums[0] = arr[1]
+//     nums[nums.length - 1] = arr[0]
+//     return nums
+// };
+// let nums = [5, 2, 3, 1]
+// let a = sortArray(nums)
+// console.log("a" ,  a)
+
